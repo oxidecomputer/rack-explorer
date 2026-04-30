6a46a1fcec34a668584075070c93f4f7e2d21c73
benjaminleonard <benji@oxide.computer>
2026-04-30 11:48:11 +0100

    Add CTA at cycled tour

--- src/components/ContactSales.tsx
+++ src/components/ContactSales.tsx
@@ -18,9 +18,8 @@ export function ContactSales({
           <div className="text-mono-xs text-tertiary flex items-center justify-between">
             Contact Sales <OpenLink12Icon className="text-quaternary" />
           </div>
-          <p className="text-default text-sans-sm mt-1 pr-2">
-            Discuss your computing requirements and business goals with our team of
-            experts.
+          <p className="text-default text-sans-sm mt-1 max-w-90 pr-2">
+            Discuss your computing requirements and business goals with our team of experts.
           </p>
         </>
       )}
